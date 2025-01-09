f47cb51073453a5bc5021f07ab78fa07f9e9d651
loicduong <mikhailcolt@gmail.com>
2025-01-09 10:06:23 +0700

    fix(projects): fix ElementPlus in GlobalComponents

--- build/plugins/unplugin.ts
+++ build/plugins/unplugin.ts
@@ -5,6 +5,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { FileSystemIconLoader } from 'unplugin-icons/loaders'
 import IconsResolver from 'unplugin-icons/resolver'
 import Icons from 'unplugin-icons/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
@@ -42,6 +43,11 @@ export function setupUnplugin(viteEnv: Env.ImportMeta) {
       dts: 'src/types/components.d.ts',
       types: [{ from: 'vue-router', names: ['RouterLink', 'RouterView'] }],
       resolvers: [
+        // auto import Element Plus components。 full import to see /src/plugins/ui.ts
+        ElementPlusResolver({
+          // no to import style, full import to see /src/plugins/assets.ts
+          importStyle: false,
+        }),
         IconsResolver({ customCollections: [collectionName], componentPrefix: VITE_ICON_PREFIX }),
       ],
     }),
